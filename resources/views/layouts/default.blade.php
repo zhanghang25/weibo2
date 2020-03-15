eb331102b6010f586241aa74fd423686f9e5cd26
hang1 <672683505@qq.com>
2020-03-15 08:46:35 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,10 +6,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title','Weibo App') - Laravel 新手入门教程</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
 <body>
-  @yield('content')
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a href="/" class="navbar-brand">Weibo App
+      </a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,5 +5,10 @@
 @endsection
 
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">你现在看到的是<a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a>的示例项目主页</p>
+    <p>一切，将从这里开始</p>
+    <p><a href="#" class="btn btn-lg btn-success">现在注册</a></p>
+</div>
 @endsection
