fcd5e177c7849e98e6d49293caba136713853b45
hang1 <672683505@qq.com>
2020-03-15 13:05:01 +0000

    用户注册页面

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UsersController extends Controller
+{
+    public function create()
+    {
+        return view('users.create');
+    }
+}
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -9,6 +9,6 @@
     <h1>Hello Laravel</h1>
     <p class="lead">你现在看到的是<a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a>的示例项目主页</p>
     <p>一切，将从这里开始</p>
-    <p><a href="#" class="btn btn-lg btn-success">现在注册</a></p>
+    <p><a href="{{ route('signup') }}" class="btn btn-lg btn-success">现在注册</a></p>
 </div>
 @endsection
