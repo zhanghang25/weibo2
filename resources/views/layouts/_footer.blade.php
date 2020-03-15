cb9abacd02b2300b49c488b346afa4db20ab1b4c
hang1 <672683505@qq.com>
2020-03-15 11:18:03 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="" class="brand-icon">
+    <a href="www.baidu.com">
+        刻意练习，每日精进
+    </a>
+    <div class="float-right"><a href="/about">关于</a></div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,18 +10,12 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a href="/" class="navbar-brand">Weibo App
-      </a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
-      </ul>
-    </div>
-  </nav>
+  @include('layouts._header')
   <div class="container">
-    @yield('content')
+    <div class="offset-md-1 col-md-10">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </div>
 </body>
 
