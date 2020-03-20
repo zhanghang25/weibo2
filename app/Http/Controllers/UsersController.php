24e544fe4eb6bedef5b41b1bd37c5e24ec31c8bf
hang1 <672683505@qq.com>
2020-03-20 14:24:16 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
