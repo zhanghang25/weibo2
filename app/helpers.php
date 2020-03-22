d13236fd0d209d4cef07bd33a8552405b1bd9334
hang1 <672683505@qq.com>
2020-03-22 14:05:05 +0000

    again error update

--- app/helpers.php
+++ app/helpers.php
@@ -4,7 +4,6 @@ function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
-        dd($url);
         return $db_config = [
             'connection' => 'pgsql',
             'host' => $url['host'],
