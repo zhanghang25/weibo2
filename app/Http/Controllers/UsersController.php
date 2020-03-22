c4eaeb3315374f8ffdaada1c02acd0ba10e70bd1
hang1 <672683505@qq.com>
2020-03-22 03:22:32 +0000

    增加csrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
@@ -14,5 +15,15 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|unique:users|max:255',
+            'password' => 'required|confirmed|min:6',
+        ]);
+        return;
     }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -11,14 +11,15 @@
         <div class="card-body">
             <form action="" method="POST"></form>
             <form action=" {{ route('users.store') }} " method="post">
+                {{ csrf_field() }}
                 <div class="form-group"><label for="name">名称:</label><input type="text" name="name" class="form-control"
-                        value=" {{ old('name') }} "></div>
+                        value="{{ old('name') }}"></div>
                 <div class="form-group"><label for="email">邮箱</label><input type="text" name="email"
-                        class="form-control" value=" {{ old('email')}} "></div>
+                        class="form-control" value="{{ old('email')}}"></div>
                 <div class="form-group"><label for="password">密码:</label><input type="password" name="password"
                         class="form-control" value="{{old('password')}}"></div>
                 <div class="form-group"><label for="password_confirmation">确认密码：</label><input type="password"
-                        name="password_confirmation" class="form-control" value="{{old('password-confirmation')}}">
+                        name="password_confirmation" class="form-control" value="{{old('password_confirmation')}}">
                 </div>
                 <button type="submit" class="btn btn-primary">注册</button>
             </form>
