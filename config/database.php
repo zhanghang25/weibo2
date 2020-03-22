3eca4b117f4e1b197878de4f9658ea8f253443f5
hang1 <672683505@qq.com>
2020-03-22 14:00:38 +0000

    again error update

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Str;
 
+$db_config = get_db_config();
 return [
 
     /*
@@ -15,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
