7d8a802862c4eddfe96af000b1913bf04d9b1fcb
hang1 <672683505@qq.com>
2020-04-01 13:02:44 +0000

    管理员删除用户1

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -7,7 +7,7 @@
     <form action="{{ route('users.destroy', $user->id) }}" method="post" class="float-right">
         {{ csrf_field() }}
         {{ method_field('DELETE') }}
-        <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
+        <button type="submit" class="btn btn-sm btn-danger">删除</button>
     </form>
     @endcan
 </div>
