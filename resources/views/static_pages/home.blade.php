a305c12fd722b3ae6abe10973b5afedc5b070d9d
hang1 <672683505@qq.com>
2020-04-05 09:43:21 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,10 +5,26 @@
 @endsection
 
 @section('content')
+@if (Auth::check())
+<div class="row">
+    <div class="col-md-8">
+        <section class="status_form">
+            @include('shared._status_form')
+        </section>
+    </div>
+    <div class="col-md-4">
+        <section class="user_info">
+            @include('shared._user_info',['user'=> Auth::user()])
+        </section>
+    </div>
+</div>
+@else
+
 <div class="jumbotron">
     <h1>Hello Laravel</h1>
     <p class="lead">你现在看到的是<a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a>的示例项目主页</p>
     <p>一切，将从这里开始</p>
     <p><a href="{{ route('signup') }}" class="btn btn-lg btn-success">现在注册</a></p>
 </div>
+@endif
 @endsection
